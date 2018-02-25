e97a27ed8fde57aa13e0404d7c7e7ec8f7ac1678
liaohongze <liaohongze@163.com>
2018-02-25 22:11:01 +0800

    添加头部的自适应

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -29,21 +29,28 @@ export default class Header extends Component {
           <li><i className='iconfont icon-qq' />0771-3924246</li>
         </ul>
         <div className='menu'>
-          <Link to='/'>首页</Link>
-          <Link to='/aboutus'>机构简介</Link>
+          <Link to='/' className='menu_item'>首页</Link>
+          <Link to='/aboutus' className='menu_item'>机构简介</Link>
           <DropdownButton
             bsStyle='default'
             title='培训课程'
-            id='menu_dropdown'
+            id='train'
           >
             <MenuItem eventKey='1'>Action</MenuItem>
             <MenuItem eventKey='2'>Another action</MenuItem>
             <MenuItem eventKey='3' active>
-        Active Item
+              Active Item
             </MenuItem>
             <MenuItem divider />
             <MenuItem eventKey='4'>Separated link</MenuItem>
           </DropdownButton>
+
+          <Link to='/translate' className='menu_item'>商务翻译</Link>
+          <Link to='/teachers' className='menu_item'>教师风采</Link>
+          <Link to='/overseas' className='menu_item'>留学签证</Link>
+          <Link to='/news' className='menu_item'>新闻资讯</Link>
+          <Link to='/course' className='menu_item'>外语课堂</Link>
+          <Link to='/contact' className='menu_item'>联系我们</Link>
         </div>
         <div className='header_logo' style={{width: headerHeight * 1.6}} />
       </div>
